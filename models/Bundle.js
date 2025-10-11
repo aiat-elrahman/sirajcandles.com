705dedd6a5ddb9905828359ccae102c1dcdba786
aiat-elrahman <1200354@eng.asu.edu.eg>
2025-10-12 00:42:04 +0300

    Update Bundle.js

--- models/Bundle.js
+++ models/Bundle.js
@@ -5,7 +5,7 @@ const bundleSchema = new mongoose.Schema(
     name: { type: String, required: true },
     description: { type: String },
     price: { type: Number, required: true },
-    products: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }], // relation with products
+    products: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }], 
     image: { type: String }, // single image
     status: { type: String, enum: ["Active", "Inactive"], default: "Active" },
     featured: { type: Boolean, default: false },
