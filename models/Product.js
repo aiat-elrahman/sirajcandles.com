631a21e3826cf7273ecce7c1a72715b5c5f921b7
aiat-elrahman <1200354@eng.asu.edu.eg>
2025-10-12 00:42:17 +0300

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -6,7 +6,7 @@ const productSchema = new mongoose.Schema(
     description: { type: String },
     price: { type: Number, required: true },
     category: { type: String },
-    images: [{ type: String }], // multiple image support
+    images: [{ type: String }], // 2-or more images
     stock: { type: Number, default: 0 },
     status: { type: String, enum: ["Active", "Inactive"], default: "Active" },
     featured: { type: Boolean, default: false },
