fc3216fccbee4a885a3af91c3fe15877535eff9b
aiat-elrahman <1200354@eng.asu.edu.eg>
2026-03-28 06:38:46 +0200

    Update Category.js

--- models/Category.js
+++ models/Category.js
@@ -1,23 +1,24 @@
 import mongoose from 'mongoose';
 
 const categorySchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true
-  },
-  image: {
-    type: String,
-    default: ''  // This will store the Cloudinary image URL
-  },
-  sortOrder: {
-    type: Number,
-    required: true,
-    default: 0
-  }
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    image: {
+        type: String,
+        default: ''
+    },
+    sortOrder: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    subcategories: [{ type: String, trim: true }]   // ← NEW: for cascaded menu
 }, {
-  timestamps: true
+    timestamps: true
 });
 
 export default mongoose.model('Category', categorySchema);
