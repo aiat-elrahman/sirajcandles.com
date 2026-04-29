a9cc3e243d653a0a60a6e10afe1ff2f5ca029203
aiat-elrahman <1200354@eng.asu.edu.eg>
2026-04-29 12:01:15 +0300

    Update Review.js
    review phone number added to module

--- models/Review.js
+++ models/Review.js
@@ -2,11 +2,13 @@ import mongoose from 'mongoose';
 
 const reviewSchema = new mongoose.Schema({
     productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
-    name:    { type: String, required: true },
-    email:   { type: String },
-    rating:  { type: Number, required: true, min: 1, max: 5 },
-    comment: { type: String },
-    photos:  [{ type: String }],
+    name:      { type: String, required: true },
+    email:     { type: String, default: '' },
+    phone:     { type: String, default: '' },
+    rating:    { type: Number, required: true, min: 1, max: 5 },
+    comment:   { type: String },
+    photos:    [{ type: String }],
+    isAdminCreated: { type: Boolean, default: false },
 }, { timestamps: true });
 
 export default mongoose.model('Review', reviewSchema);
